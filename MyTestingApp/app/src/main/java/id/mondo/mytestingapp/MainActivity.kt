1759c528edf3dd99a765145303198982388e3c46
Raqhib <raqhib.ataillah@gmail.com>
2025-01-09 21:29:29 +0800

    push MyRecyclerView MyService MySettingPreference MySharedPreferences mySound MyStackWidget MyTabLayout MyTestingApp MyUnitTest MyViewAndViewGroup MyViewModel MyWebView MyWidgets

---
+++ MyTestingApp/app/src/main/java/id/mondo/mytestingapp/MainActivity.kt
@@ -0,0 +1,41 @@
+package id.mondo.mytestingapp
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnSetValue: Button
+    private lateinit var tvText: TextView
+
+    private var names = ArrayList<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvText = findViewById(R.id.tv_text)
+        btnSetValue = findViewById(R.id.btn_set_value)
+        btnSetValue.setOnClickListener(this)
+
+        names.add("Rahib Ataillah")
+        names.add("Rai")
+        names.add("Mondo")
+
+    }
+
+    override fun onClick(view: View?) {
+        if (view?.id == R.id.btn_set_value){
+            Log.d("MainActivity", names.toString())
+            val name = StringBuilder()
+            for (i in 0..2){
+                name.append(names[i]).append("\n")
+            }
+            tvText.text = name.toString()
+        }
+    }
+}
